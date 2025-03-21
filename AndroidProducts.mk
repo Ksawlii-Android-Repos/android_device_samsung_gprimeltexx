dd2f16e63093fb2c48484271a6dc66e22c8047e6
Ksawlii <ksawery.blaszczak@proton.me>
2025-03-21 11:40:13 +0100

    gprimeltexx: HavocOS Time
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,3 @@
 LOCAL_PATH := device/samsung/gprimeltexx
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_gprimeltexx.mk
-
-COMMON_LUNCH_CHOICES := \
-	lineage_gprimeltexx-eng \
-	lineage_gprimeltexx-userdebug \
-	lineage_gprimeltexx-user
+	$(LOCAL_DIR)/havoc_gprimeltexx.mk
